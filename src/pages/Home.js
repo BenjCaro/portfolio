2c6c39bd5a416e7301b8a4810a7d33680b257477
BenjCaro <carbenjamin@gmail.com>
2025-02-16 18:41:04 +0100

    creation section présentation, section a propos et competence, ajustement, réalisation des barres progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import './home.css';
+
+const TitleHome = () => <h1 className='fw-bold'>Developpeur web full stack</h1> ;
+const Hello = (props) => <h2 className='fw-bold'>Bonjour, je suis {props.surname} {props.name}</h2>;
+const ButtonHome = () => <button type="button" className="btn btn-danger btn-sm">En savoir plus</button> ;
+const Progress = (props) => <h4>{props.langage}</h4>;
+        
+    
+
 
 const Home = () => {
     return (
-        <div>
-            <h1>
-                PAGE HOME
-            </h1>
-        </div>
+        
+        <>
+            <div className="container-fluid  img-bg d-flex justify-content-center align-items-center">
+                <div className='text-center text-white'>
+                    <Hello surname="John" name="Doe" />
+                    <TitleHome />
+                    <ButtonHome />
+                </div>
+            </div>
+            <div className='container bg-body'>
+                <div className='row border border-white m-4 shadow rounded-1'>
+                    <section className='col-md-6 py-3 ps-4'>
+                        <h3 className='border-bottom border-primary border-4 fw-bold pb-2'>
+                            A propos
+                        </h3>
+                    </section>
+                    <section className='col-md-6 py-3 ps-4 pe-4'>
+                        <h3 className='border-bottom border-primary border-4 fw-bold pb-2'>
+                           Mes Compétences
+                        </h3>
+                        <div className='mt-4 mb-4'>
+                            <Progress langage="HTML5 90%" />
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar bg-danger" style={{width: "90%"}}></div>
+                            </div>
+                        </div>
+                        <div className='mt-4 mb-4'>
+                            <Progress langage="CSS3 80%" />
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar bg-info" style={{width: "80%"}}></div>
+                            </div>
+                        </div>
+                        <div className='mt-4 mb-4'>
+                            <Progress langage="JAVASCRIPT 70%" />
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar bg-warning" style={{width: "70%"}}></div>
+                            </div>
+                        </div>
+                        <div className='mt-4 mb-4'>
+                            <Progress langage="PHP 60%"/>
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar bg-success" style={{width: "60%"}}></div>
+                            </div>
+                        </div>
+                        <div className='mt-4 mb-4'>
+                            <Progress langage="REACT 50%" />
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar bg-blue" style={{width: "50%"}}></div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </>
+        
+        
     )
 }
 
