eac8cd95611cf7ca313dd83de7a4312b0fc2481c
BenjCaro <carbenjamin@gmail.com>
2025-02-18 19:02:59 +0100

    mise en place d'id pour chaque card

--- src/pages/Component/Card.js
+++ src/pages/Component/Card.js
@@ -1,12 +1,12 @@
 const Card = (props) => {
     return (
 
-        <div className="card col text-center m-auto" style={{width: "18rem"}}>
+        <div id={props.identifiant} className="card col text-center m-auto" style={{width: "18rem"}}>
             <img src={props.source} className="card-img-top img-fluid" alt={props.description}></img>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.text}</p>
-                <a href="#" className="btn btn-primary">Voir le site</a>
+                <a href={props.url} className="btn btn-primary">Voir le site</a>
             </div>
             <div className="card-footer">
                 {props.langage} 
--- src/pages/Realisations.js
+++ src/pages/Realisations.js
@@ -9,26 +9,25 @@ const Realisations = () => {
             <Heading title="Portfolio" subtitle="Voici quelques unes de mes réalisations" />
            <div className="container">
                 <div className="content d-flex  justify-content-center">
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/fresh-food.jpg" description="panier de légumes" title="Fresh Food" text="Site de produits frais en ligne" langage="Site réalisé avec PHP et MySQL"/>
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="one" url="/pages/realisations/#one" source="/images/portfolio/fresh-food.jpg" description="panier de légumes" title="Fresh Food" text="Site de produits frais en ligne" langage="Site réalisé avec PHP et MySQL"/>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/restaurant-japonais.jpg" description="plateau sushis" title="Restaurant Akira" text="Site de produits frais en ligne" langage="Site réalisé avec WordPress"/>
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="two" url="/pages/realisations/#two"  source="/images/portfolio/restaurant-japonais.jpg" description="plateau sushis" title="Restaurant Akira" text="Site de produits frais en ligne" langage="Site réalisé avec WordPress"/>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/espace-bien-etre.jpg" description="ambiance zen" title="Espace Bien Etre" text="Site de produits frais en ligne" langage="Site réalisé avec LARAVEL"/>
-                    </div>
-                    
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="three" url="/pages/realisations/#three" source="/images/portfolio/espace-bien-etre.jpg" description="ambiance zen" title="Espace Bien Etre" text="Site de produits frais en ligne" langage="Site réalisé avec LARAVEL"/>
+                    </div>  
                 </div>   
-                <div className="content d-flex  justify-content-center ">
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/seo.jpg" description="seo" title="SEO" text="Amélioration du référencement d'un site web" langage="Utilisations des outils SEO"/>
+                <div className="content d-flex  justify-content-center mb-4 ">
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="four" url="/pages/realisations/#four" source="/images/portfolio/seo.jpg" description="seo" title="SEO" text="Amélioration du référencement d'un site web" langage="Utilisations des outils SEO"/>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/coder.jpg" description="lignes de code" title="Création d'une API" text="Création d'une API RESTFULL publique" langage="PHP - SYMFONY"/>
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="five" url="/pages/realisations/#five" source="/images/portfolio/coder.jpg" description="lignes de code" title="Création d'une API" text="Création d'une API RESTFULL publique" langage="PHP - SYMFONY"/>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-4 ms-4 mb-4">
-                        <Card source="/images/portfolio/screens.jpg" description="laptop sur un bureau" title="Maquette d'un site web" text="Création d'un prototype de site" langage="Réalisé avec Figma"/>    
+                    <div className="col-lg-4 col-md-4 col-sm-12 d-flex justify-content-center me-2 ms-2 mb-4">
+                        <Card identifiant="six" url="/pages/realisations/#six" source="/images/portfolio/screens.jpg" description="laptop sur un bureau" title="Maquette d'un site web" text="Création d'un prototype de site" langage="Réalisé avec Figma"/>    
                     </div>
                 </div>  
             </div>
