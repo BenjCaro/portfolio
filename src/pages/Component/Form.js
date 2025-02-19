0ad1ee5d85a1ff7198b88ae102461c9368a784a9
BenjCaro <carbenjamin@gmail.com>
2025-02-19 19:22:41 +0100

    mise en place de la responsivite

--- src/pages/Component/Form.js
+++ src/pages/Component/Form.js
@@ -3,41 +3,22 @@ const Form = () => {
         <form className="mt-4">
             <div class="row text-center">
                 <div class="col-md-12 mb-2">
-                    
                     <input type="text" class="form-control" name="nom" placeholder="Votre nom" required/>
-                
                 </div>
-                
                 <div class="col-md-12 mb-2">
-                    
                     <input type="email" class="form-control" name="email" placeholder="Votre adresse mail" required/>
-                
-                </div>
-                
-                
+                 </div>
                 <div class="col-md-12 mb-2">
-                    
                     <input type="tel" class="form-control" name="phone" placeholder="Votre numéro de téléphone" required/>
-                    
                 </div>
                 <div class="col-md-12 mb-2">
-                    
-                    <input type="text" class="form-control" name="sujet" placeholder="Sujet" required/>
-                    
+                    <input type="text" class="form-control" name="sujet" placeholder="Sujet" required/> 
                 </div>
-                
-                
                 <div class="col-md-12 mb-3">
-                    
                     <textarea class="form-control" placeholder="Votre Message" rows="14" required></textarea>
-                
                 </div>
-                
-                
                 <div class="col-md-12 mb-3">
-                    
                     <input type="submit" class="btn btn-primary" value="Envoyer"/>
-                
                 </div>
             </div>
       </form>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,14 +15,14 @@ const Contact = () => {
     return (
         <>
         <Heading title="Contact" subtitle="Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact." />
-        <section className='container border d-flex mt-4 mb-4'>
-            <div className='m-2 p-2'>
+        <section className='container content border d-flex mt-4 mb-4'>
+            <div className='m-2 p-2 col-lg-6 col-md-6 col-sm-12'>
                 <h2 className='border-bottom border-primary border-4 fw-bold pb-2 fs-4 '>
                     Formulaire de contact
                 </h2>
                 <Form/>
             </div>
-            <div className='m-2 p-2'>
+            <div className='m-2 p-2 col-lg-6 col-md-6 col-sm-12'>
                 <h2 className='border-bottom border-primary border-4 fw-bold pb-2 fs-4'>
                    Mes Coordonnées
                 </h2>
@@ -34,8 +34,8 @@ const Contact = () => {
                         <i class="bi bi-phone"></i> {user.phone} <br/>
                         <i class="bi bi-envelope-at"></i> {user.mail}
                     </div>
-                    <div className='mt-2'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11130.305869243979!2d4.794157184538999!3d45.77967760886546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1739985401143!5m2!1sfr!2sfr" title="Google map localisation" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div className='mt-2 overflow-hidden'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11130.305869243979!2d4.794157184538999!3d45.77967760886546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1739985401143!5m2!1sfr!2sfr" title="Google map localisation" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
