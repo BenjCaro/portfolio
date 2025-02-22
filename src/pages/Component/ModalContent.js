9e7a60c480210de6060ff2c6559a95f80b78eac4
BenjCaro <carbenjamin@gmail.com>
2025-02-22 15:04:26 +0100

    reajustement mise en page de la modale

--- src/pages/Component/ModalContent.js
+++ src/pages/Component/ModalContent.js
@@ -16,13 +16,14 @@ const ModalContent = ({ onClose }) => {
 
      return (
         <>
-        <div className="card text-bg-dark position-fixed top-50 start-50 translate-middle w-100 d-flex justify-content-center align-items-center m-sm-4" data-bs-theme="dark" style={{maxWidth: "540px"}}>
-           <div className="card-body mb-3">
-                <div className="d-flex justify-content-between border-bottom border-secondary-subtle">
+        <div className="card text-bg-dark position-fixed top-50 start-50 translate-middle w-100 m-sm-4" data-bs-theme="dark" style={{maxWidth: "650px"}}>
+           <div className="card-header">
+                <div className="d-flex justify-content-between ">
                     <h2 className="fs-4">Mon profil Github</h2> 
                     <button type="button" class="btn-close" onClick={onClose} ></button>
                 </div>
-                <div className="row g-0">
+            </div>  
+            <div className="row">
                     <div className="col-md-4 mt-3">
                         <img src={user.avatar_url} className="img-fluid rounded-start" alt={user.login}/>
                     </div>
@@ -33,15 +34,16 @@ const ModalContent = ({ onClose }) => {
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-card-text"></i>{user.bio}</p>
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-box"></i>Repositories: {user.public_repos}</p>
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-people"></i>Followers: {user.followers}</p>
-                            <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-people"></i>Followings: {user.following}</p>
+                            <p className="card-text"><i class="i-modal bi bi-people"></i>Followings: {user.following}</p>
                         </div>
                     </div>
-                </div>
             </div>
             <div className="card-footer text-end">
                 <button className="btn btn-secondary" onClick={onClose}>Fermer</button>
             </div>
-        </div>
+         </div>
+            
+        
         </>
         
      )
@@ -49,3 +51,4 @@ const ModalContent = ({ onClose }) => {
 
 
 export default ModalContent;
+
