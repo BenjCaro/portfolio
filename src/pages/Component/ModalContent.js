40dffcb0b0b81ef5f576a77bfa62f84fcae757fa
BenjCaro <carbenjamin@gmail.com>
2025-02-22 15:15:38 +0100

    cloture issu

--- src/pages/Component/ModalContent.js
+++ src/pages/Component/ModalContent.js
@@ -16,20 +16,20 @@ const ModalContent = ({ onClose }) => {
 
      return (
         <>
-        <div className="card text-bg-dark position-fixed top-50 start-50 translate-middle w-100 m-sm-4" data-bs-theme="dark" style={{maxWidth: "650px"}}>
+        <div className="card text-bg-dark position-fixed top-50 start-50 translate-middle w-100 m-sm-4" data-bs-theme="dark" style={{maxWidth: "540px"}}>
            <div className="card-header">
                 <div className="d-flex justify-content-between ">
                     <h2 className="fs-4">Mon profil Github</h2> 
                     <button type="button" class="btn-close" onClick={onClose} ></button>
                 </div>
             </div>  
-            <div className="row">
-                    <div className="col-md-4 mt-3">
+            <div className="row m-2">
+                    <div className="col-md-7 mt-3">
                         <img src={user.avatar_url} className="img-fluid rounded-start" alt={user.login}/>
                     </div>
-                    <div className="col-md-8 mt-3">
+                    <div className="col-md-5 mt-3">
                         <div className="card-body">
-                            <h5 className="card-title border-bottom border-secondary-subtle"><i class="i-modal bi bi-person"></i>{user.name}</h5>
+                            <h5 className="card-title border-bottom border-secondary-subtle"><i class="i-modal bi bi-person"></i><a href="https://github.com/" target='_blank' rel="noreferrer">{user.name}</a></h5>
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-geo-alt"></i>{user.location}</p>
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-card-text"></i>{user.bio}</p>
                             <p className="card-text border-bottom border-secondary-subtle"><i class="i-modal bi bi-box"></i>Repositories: {user.public_repos}</p>
